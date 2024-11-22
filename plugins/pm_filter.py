46815884aabc0684d730aedbdbc2f7e0842f76ab
Mahesh8188 <ddbambhava8182@gmail.com>
2024-11-22 18:10:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2656,7 +2656,7 @@ async def auto_filter(client, msg, spoll=False):
     ])
     btn.insert(0,
     [ 
-    InlineKeyboardButton(text="⚡ ʜᴏᴡ ᴛᴏ ᴅᴏᴡɴʟᴏᴀᴅ ⚡", url='https://telegram.me/vegamoviesnewin'),
+    InlineKeyboardButton(text="⚡ WITHOUT AD DOWNLOAD ⚡", url='https://telegram.me/vegamoviesnewin'),
     ])
 
     if offset != "":
